0d9ce92362b822bebbd12b32fd7ee71237497bba
Armaan Chahal <armaan.c2009@gmail.com>
2025-08-30 16:50:07 -0500

    Add vercel analytics package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,26 @@
-import type { Metadata } from 'next';
-import { ViewTransitions } from "next-view-transitions";
+import type {Metadata} from 'next';
+import {ViewTransitions} from "next-view-transitions";
 import ThemeControl from "./ThemeControl";
 import "./globals.css";
 
+import {Analytics} from "@vercel/analytics/next"
+
 export const metadata: Metadata = {
-  title: "Armaan Chahal",
-  description: "Personal website and blog of Armaan Chahal.",
+    title: "Armaan Chahal",
+    description: "Personal website and blog of Armaan Chahal.",
 };
 
 export default function RootLayout({
                                        children,
-}: Readonly<{
+                                   }: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
         <ViewTransitions>
             <html lang="en" className="box-border m-0 dark">
             <head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
                 {/* eslint-disable-next-line @next/next/google-font-display, @next/next/no-page-custom-font */}
                 <link
                     href="https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,100..900&family=Libre+Baskerville:ital@1&display=block"
@@ -30,20 +32,21 @@ export default function RootLayout({
                     href="/favicon-96x96.png"
                     sizes="96x96"
                 />
-                <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-                <link rel="shortcut icon" href="/favicon.ico" />
+                <link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
+                <link rel="shortcut icon" href="/favicon.ico"/>
                 <link
                     rel="apple-touch-icon"
                     sizes="180x180"
                     href="/apple-touch-icon.png"
                 />
-                <meta name="apple-mobile-web-app-title" content="MyWebSite" />
-                <link rel="manifest" href="/site.webmanifest" />
+                <meta name="apple-mobile-web-app-title" content="MyWebSite"/>
+                <link rel="manifest" href="/site.webmanifest"/>
             </head>
             <body className="antialiased bg-background text-foreground font-sans">
 
             <ThemeControl>
                 {children}
+                <Analytics/>
                 <footer className="text-muted font-sans mt-16 py-8 text-center">
                     All Rights Reserved.
                 </footer>
