11d88ece6686e14a226ab09006c0e9ebf0a8ff6b
Armaan Chahal <armaan.c2009@gmail.com>
2025-08-30 20:03:49 -0500

    Fix black border around the code segments in Pre component (just had to remove one css class "bg-background")

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -180,8 +180,14 @@ export function Pre({
                     }: React.HTMLAttributes<HTMLPreElement>) {
     const lang = className?.split("language-")[1] ?? "ts";
     return (
-        <div className="my-md rounded bg-black/80 p-md overflow-x-auto border-0 outline-0 ring-0 shadow-none">
-            <Code lang={lang} theme="github-dark" {...props} />
+        <div className="my-md rounded p-md overflow-x-auto">
+            <div>
+                <Code
+                    lang={lang}
+                    theme="github-dark"
+                    {...props}
+                />
+            </div>
         </div>
     );
 }
