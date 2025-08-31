250f136da52b01ab3b44b6996ad4d3cd4202ad5f
Armaan Chahal <armaan.c2009@gmail.com>
2025-08-30 21:10:50 -0500

    Add portfolio website to nav bar and main page

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -3,12 +3,18 @@ import { Link } from "next-view-transitions";
 
 export function Nav() {
     return (
-        <nav className="absolute top-2 left-0 flex gap-2 p-4 w-auto">
+        <nav className="absolute top-2 left-0 flex gap-6 p-4 w-auto">
             <Link
                 href="/blog"
                 className="text-[crimson] dark:text-[cornflowerblue] uppercase no-underline hover:underline underline-offset-8"
             >
                 Blog
+            </Link>
+            <Link
+                href="https://vportfolio-hazel.vercel.app/"
+                className="text-[crimson] dark:text-[cornflowerblue] uppercase no-underline hover:underline underline-offset-8"
+            >
+                Portfolio
             </Link>
         </nav>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Logo from "./Logo";
 import { Link } from "next-view-transitions";
+import React from "react";
 
 export default function Home() {
     return (
@@ -7,12 +8,18 @@ export default function Home() {
             <header className="flex flex-col items-center justify-center gap-8 min-h-[90vh] sm:min-h-[90dvh] py-4">
                 <Logo className="w-[calc(100%-32px)] max-w-[800px] translate-x-[1.5%]" collapsible />
 
-                <nav className="flex gap-12">
+                <nav className="flex gap-16">
                     <Link
                         href="/blog"
                         className="text-[crimson] dark:text-[cornflowerblue] uppercase no-underline hover:underline underline-offset-8 w-16 text-center"
                     >
                         Blog
+                    </Link>
+                    <Link
+                        href="https://vportfolio-hazel.vercel.app/"
+                        className="text-[crimson] dark:text-[cornflowerblue] uppercase no-underline hover:underline underline-offset-8 w-16 text-center"
+                    >
+                        Portfolio
                     </Link>
                 </nav>
             </header>
