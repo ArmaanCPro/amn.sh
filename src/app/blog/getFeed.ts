d2e1b2d3dc57c548681100349a6219f43f205792
Armaan Chahal <armaan.c2009@gmail.com>
2025-09-04 20:14:50 -0500

    Change URL to amn.sh

--- src/app/blog/getFeed.ts
+++ src/app/blog/getFeed.ts
@@ -3,7 +3,7 @@ import { getBlogPosts} from "./getPosts";
 import { unstable_cache } from "next/cache";
 
 export const getFeed = unstable_cache(async function getFeed() {
-    const siteURL = "https://amn-sh.vercel.app";
+    const siteURL = "https://www.amn.sh";
     const feedOptions = {
         title: "amn.sh",
         language: "en",
